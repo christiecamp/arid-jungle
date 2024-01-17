f9a58bd8d15a5217a1003284cd5a443cb34c0568
christiecamp <christiecamphoto@gmail.com>
2024-01-17 17:13:55 -0500

    more debugging and minor updates

--- client/src-sw.js
+++ client/src-sw.js
@@ -6,7 +6,9 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+//array of URLs to precache
 precacheAndRoute(self.__WB_MANIFEST);
+
 //page caching
 const pageCache = new CacheFirst({
     cacheName: 'page-cache',
@@ -29,18 +31,19 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 //asset caching
 registerRoute(
-    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-    new StaleWhileRevalidate({
-        //name cache storage
-        cacheName: 'assets-cache',
-        plugins: [
-            //cacheable response plugin - 30 day max age
-            new CacheableResponsePlugin({
-                statuses: [0, 200],
-            }),
-        ],
-    }),
-);
+    ({request}) => request.destination === 'image',
+    new CacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new ExpirationPlugin({
+          //weekly cache
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+          //10 requests max
+          maxEntries: 10,
+        }),
+      ],
+    })
+  );
 
 //offline fallback
 // offlineFallback();
